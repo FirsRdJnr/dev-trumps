c77d610ba9f021870bba536b7e3e46db32bed138
FirsRdJnr <victoria.baron@hotmail.co.uk>
2014-02-07 17:39:28 +0000

    adding json

---
+++ people.js
@@ -0,0 +1,31 @@
+// http://iviewsource.com/codingtutorials/getting-started-with-javascript-object-notation-json-for-absolute-beginners/
+[
+     {
+        "Name": "Tom Chapman",
+        "Start" : "17/07/2012",
+        "Team" : "What the Frog",
+        "Position" : "Tech Lead & Team Captain",
+        "Specialities" : "PhP, git"
+    },
+    {
+        "name": "V Baron",
+        "Start" : "11/10/2011",
+        "Team" : "Architecture",
+        "Position" : "Imp Arch",
+        "Specialities" : "CSS, HTML"
+    },
+    {
+        "name": "Lee Mills",
+        "Start" : "01/03/2012",
+        "Team" : "Architecture",
+        "Position" : "Architecture Team Lead",
+        "Specialities" : "JS, IOS, Frontend, Man flu"
+    },
+    {
+        "name": "Dangerous Judas Dave",
+        "Start" : "18/01/2013",
+        "Team" : "Architecture",
+        "Position" : "Ex-Soloutions Arch",
+        "Specialities" : "Cycling, Stress ball deconstruction"
+    },
+]
