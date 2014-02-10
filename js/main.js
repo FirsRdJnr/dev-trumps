61231d999907ac9f212fd30cc8914364a0ec5ea6
FirsRdJnr <victoria.baron@hotmail.co.uk>
2014-02-10 17:32:16 +0000

    final styling

--- js/main.js
+++ js/main.js
@@ -8,61 +8,57 @@ var latest = "https://github.com/barrel/mixitup/archive/v1.5.5.zip";
 /* SHARED VARS */
 
 var firstrun = true,
-	liveEffects = ['fade','scale'],
-	liveEasing = 'smooth',
-	liveSpeed = 500,
-	touch = false,
-	list = false,
-	clickEv = 'click';
+    liveEffects = ['fade','scale'],
+    liveEasing = 'smooth',
+    liveSpeed = 500,
+    touch = false,
+    list = false,
+    clickEv = 'click';
 
-/* SANDBOX */
+function mixdevs(){
 
-function mixSandBox(){
-	
-	// INSTANTIATE MIXITUP ON SANDBOX
-	
-	$('#SandBox').mixitup({
-		buttonEvent: clickEv,
-		onMixStart: function(config){
-			
-			// PAUSE LOGO ON SANDBOX ACTIVITY
-			if(typeof timer != 'undefined'){
-				clearInterval(timer);
-			};
-			if(typeof counter != 'undefined'){
-				clearInterval(counter);
-			};
-			
-			// UPDATE EFFECTS LIST
-			config.effects = liveEffects;
-			
-			// UPDATE EASING
-			config.easing = liveEasing;
-			
-			// UPDATE SPEED
-			config.transitionSpeed = liveSpeed;
-				
-			return config;
-		},
-		onMixEnd: function(config){
-			
-			// ADD LIST STYING
-			var wait = setTimeout(function(){
-				if(config.layoutMode == 'list'){
-					list = true;
-					$('#SandBox .mix').addClass('full_width');
-				};
-			},100);
-		}
-	});
+    // INSTANTIATE MIXITUP ON devs
+    $('#devs').mixitup({
+        buttonEvent: clickEv,
+        onMixStart: function(config){
+
+            // PAUSE LOGO ON devs ACTIVITY
+            if(typeof timer != 'undefined'){
+                clearInterval(timer);
+            };
+            if(typeof counter != 'undefined'){
+                clearInterval(counter);
+            };
+
+            // UPDATE EFFECTS LIST
+            config.effects = liveEffects;
+
+            // UPDATE EASING
+            config.easing = liveEasing;
+
+            // UPDATE SPEED
+            config.transitionSpeed = liveSpeed;
+
+            return config;
+        },
+        onMixEnd: function(config){
+
+            // ADD LIST STYING
+            var wait = setTimeout(function(){
+                if(config.layoutMode == 'list'){
+                    list = true;
+                    $('#devs .mix').addClass('full_width');
+                };
+            },100);
+        }
+    });
 }
 
 /* BACK TO TOP BUTTON */
-
 function backToTop(){
-	var ww = $(window).width();
-	var bttMar = ((ww - 860) / 2) - 80;
-	$('#BackToTop').css('right',bttMar+'px')	
+    var ww = $(window).width();
+    var bttMar = ((ww - 860) / 2) - 80;
+    $('#BackToTop').css('right',bttMar+'px')
 }
 
 
@@ -71,55 +67,46 @@ function backToTop(){
 
 function eventHandlers(){
 
-	/* GRID/LIST */
-	
-	$('#GoList').bind(clickEv, function(e){
-		$('.button.layout').removeClass('active');
-		$(this).addClass('active');
-		$('#SandBox').mixitup('toList');
-	});
-	
-	$('#GoGrid').bind(clickEv, function(e){
-		if(list){
-			$('.button.layout').removeClass('active');
-			$(this).addClass('active');
-			var delay = setTimeout(function(){
-				$('#SandBox .mix').removeClass('full_width');
-				$('#SandBox').mixitup('toGrid');
-			});
-		};
-	});
-	
+    /* GRID/LIST */
+    $('#GoList').bind(clickEv, function(e){
+        $('.button.layout').removeClass('active');
+        $(this).addClass('active');
+        $('#devs').mixitup('toList');
+    });
+
+    $('#GoGrid').bind(clickEv, function(e){
+        if(list){
+            $('.button.layout').removeClass('active');
+            $(this).addClass('active');
+            var delay = setTimeout(function(){
+                $('#devs .mix').removeClass('full_width');
+                $('#devs').mixitup('toGrid');
+            });
+        };
+    });
+
 }
-
-
-
-
 
 /* ====== ON DOCU READY ====== */
 
 $(function(){
-	
-	/* DETECT PLATFORM */
-	
-	$.support.touch = 'ontouchend' in document;
 
-	if ($.support.touch) {
-		touch = true;
-		$('body').addClass('touch');
-		clickEv = 'touchclick';
-	};
-	
-	/* POSITION BACK TO TOP BUTTON */
-	
-	backToTop();
-	
-	/* INSTANTIATE SANDBOX */
-	
-	mixSandBox();
-	
-	/* EVENT HANDLERS */
-	
-	eventHandlers();
-	
+    /* DETECT PLATFORM */
+    $.support.touch = 'ontouchend' in document;
+
+    if ($.support.touch) {
+        touch = true;
+        $('body').addClass('touch');
+        clickEv = 'touchclick';
+    };
+
+    /* POSITION BACK TO TOP BUTTON */
+    backToTop();
+
+    /* INSTANTIATE devs */
+    mixdevs();
+
+    /* EVENT HANDLERS */
+    eventHandlers();
+
 });
